eba56bc284c22f16424b8579f17368717c8c9cb6
bentoluizv <bentoluizv@gmail.com>
2024-10-21 09:54:40 -0300

    add nav component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <nav className="text-sm flex flex-col">
+      <a
+        className="bg-blue-500 text-white text-lg py-4 px-8 md:py-2 md:px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-center"
+        href="./cadastro.html">
+        Nova Reserva
+      </a>
+    </nav>
+  );
+}
