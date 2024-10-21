a841ff030c3dbd9e31d97846323e09a5bfdd8962
bentoluizv <bentoluizv@gmail.com>
2024-10-21 09:55:44 -0300

    add booking list component

---
+++ src/components/BookingList.tsx
@@ -0,0 +1,9 @@
+export default function BookingList() {
+  return (
+    <div className="flex flex-col h-[440px] md:h-[570px] lg:h-[580px] xl:h-[740px]">
+      <ul
+        id="listagem"
+        className="flex flex-col mt-4 gap-8 md:grid md:grid-cols-2 lg:grid-cols-3  h-full overflow-auto"></ul>
+    </div>
+  );
+}
