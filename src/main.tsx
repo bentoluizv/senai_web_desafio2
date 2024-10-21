1f337c82166b65854c7664f816f99804784162cb
bentoluizv <bentoluizv@gmail.com>
2024-10-21 09:56:48 -0300

    add main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Olá</h1>
+    <Container>
+      <Header />
+      <Main />
+    </Container>
+    <Footer />
   </StrictMode>
 );
