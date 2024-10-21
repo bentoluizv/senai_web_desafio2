9fa57195087254bf92d1f94625c4401c698ef71e
bentoluizv <bentoluizv@gmail.com>
2024-10-21 09:54:28 -0300

    add header title component

---
+++ src/components/ui/HeaderTitle.tsx
@@ -0,0 +1,9 @@
+export default function HeaderTitle() {
+  return (
+    <a href="/">
+      <h2 className="text-3xl text-center">
+        Gerenciador de Vagas de Estacionamento
+      </h2>
+    </a>
+  );
+}
