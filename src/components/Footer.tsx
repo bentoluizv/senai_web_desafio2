e69aa65c56c0194bd7ece60ba5d7fe11da7c30b0
bentoluizv <bentoluizv@gmail.com>
2024-10-21 09:56:03 -0300

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col bg-blue-600 text-gray-200 h-[60px] justify-center">
+      <p className="text-center">developed by @bentoluizv</p>
+    </footer>
+  );
+}
