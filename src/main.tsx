b26a37eeb6816312df84a76ffb1279c8ce17333e
bentoluizv <bentoluizv@gmail.com>
2024-10-22 10:34:35 -0300

    add load sample data func call

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Index from "./pages/Index";
+import loadSampleData from "./utils/loadSampleData";
+
+loadSampleData();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
