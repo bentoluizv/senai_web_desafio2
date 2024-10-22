70c4ddac8be3a68405493552c9832034323f7bfb
bentoluizv <bentoluizv@gmail.com>
2024-10-21 23:38:00 -0300

    add form submit btn component

---
+++ src/components/ui/SubmitButton.tsx
@@ -0,0 +1,10 @@
+export default function SubmitButton() {
+  return (
+    <button
+      id="btn-submit"
+      className="font-bold text-xl bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      type="submit">
+      Registrar
+    </button>
+  );
+}
