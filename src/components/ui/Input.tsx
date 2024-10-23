ee75f46a9dc287d781ef9147906bfe568020abe5
bentoluizv <bentoluizv@gmail.com>
2024-10-23 00:21:56 -0300

    add to input props hook form register and errors

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,17 +1,31 @@
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
 type Props = {
   type: string;
   name: string;
   isRequired: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
 };
 
-export default function input({ type, name, isRequired }: Props) {
+export default function input({
+  type,
+  name,
+  isRequired,
+  register,
+  errors,
+}: Props) {
   return (
-    <input
-      className="border border-gray-300 rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      type={type}
-      name={name}
-      id={name}
-      required={isRequired}
-    />
+    <>
+      <input
+        {...register(name)}
+        className="border border-gray-300 rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        type={type}
+        name={name}
+        id={name}
+        required={isRequired}
+      />
+      {errors.root?.message && <span>{errors.root.message}</span>}
+    </>
   );
 }
