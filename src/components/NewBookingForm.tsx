bff81ad5ef94ef84d1fd30483f217776c83cd3d9
bentoluizv <bentoluizv@gmail.com>
2024-10-23 00:22:26 -0300

    add hook form and zod resolver to form

--- src/components/NewBookingForm.tsx
+++ src/components/NewBookingForm.tsx
@@ -1,67 +1,106 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ReactNode } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { ReservaSchema } from "../models/ReservaModel";
+import Input from "./ui/Input";
 import InputGroup from "./ui/InputGroup";
+import Label from "./ui/Label";
 import LabelInput from "./ui/LabelInput";
 
 type Props = {
   children: ReactNode;
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  onSubmit: (formData: FieldValues) => void;
 };
 
 export default function NewBookingForm({ children, onSubmit }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(ReservaSchema),
+  });
+
   return (
     <form
-      onSubmit={onSubmit}
+      onSubmit={handleSubmit((formData) => onSubmit(formData))}
       id="register-form"
       className="flex flex-col items-center">
       <div className="flex flex-col mt-4 gap-4 ">
         <InputGroup>
-          <LabelInput
-            type={"text"}
-            name={"proprietario"}
-            content={"Proprietario"}
-            isRequired={true}
-          />
-          <LabelInput
-            type={"text"}
-            name={"apartamento"}
-            content={"Apartamento"}
-            isRequired={true}
-          />
+          <LabelInput>
+            <Label input={"proprietario"} content={"Proprietario"} />
+            <Input
+              type={"text"}
+              name={"proprietario"}
+              isRequired={true}
+              register={register}
+              errors={errors}
+            />
+          </LabelInput>
+          <LabelInput>
+            <Label input={"apartamento"} content={"Apartamento"} />
+            <Input
+              type={"text"}
+              name={"apartamento"}
+              isRequired={true}
+              register={register}
+              errors={errors}
+            />
+          </LabelInput>
         </InputGroup>
         <InputGroup>
-          <LabelInput
-            type={"text"}
-            name={"bloco"}
-            content={"Bloco"}
-            isRequired={true}
-          />
-          <LabelInput
-            type={"text"}
-            name={"placa"}
-            content={"Placa"}
-            isRequired={true}
-          />
+          <LabelInput>
+            <Label input={"bloco"} content={"Bloco"} />
+            <Input
+              type={"text"}
+              name={"bloco"}
+              isRequired={true}
+              register={register}
+              errors={errors}
+            />
+          </LabelInput>
+          <LabelInput>
+            <Label input={"placa"} content={"Placa"} />
+            <Input
+              type={"text"}
+              name={"placa"}
+              isRequired={true}
+              register={register}
+              errors={errors}
+            />
+          </LabelInput>
         </InputGroup>
         <InputGroup>
-          <LabelInput
-            type={"text"}
-            name={"modelo"}
-            content={"Modelo"}
-            isRequired={true}
-          />
-          <LabelInput
-            type={"text"}
-            name={"vaga"}
-            content={"Vaga"}
-            isRequired={true}
-          />
+          <LabelInput>
+            <Label input={"modelo"} content={"Modelo"} />
+            <Input
+              type={"text"}
+              name={"modelo"}
+              isRequired={true}
+              register={register}
+              errors={errors}
+            />
+          </LabelInput>
+          <LabelInput>
+            <Label input={"vaga"} content={"Vaga"} />
+            <Input
+              type={"text"}
+              name={"vaga"}
+              isRequired={true}
+              register={register}
+              errors={errors}
+            />
+          </LabelInput>
         </InputGroup>
         <InputGroup>
-          <LabelInput
+          <Label input={"cor"} content={"Cor"} />
+          <Input
             type={"text"}
             name={"cor"}
-            content={"Cor"}
             isRequired={true}
+            register={register}
+            errors={errors}
           />
         </InputGroup>
       </div>
