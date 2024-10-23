5e5852ba650f79f0b2446bb119d4973e85302247
bentoluizv <bentoluizv@gmail.com>
2024-10-23 00:21:24 -0300

    label and input as element children

--- src/components/ui/LabelInput.tsx
+++ src/components/ui/LabelInput.tsx
@@ -1,18 +1,9 @@
-import Input from "./Input";
-import Label from "./Label";
+import { ReactNode } from "react";
 
 type Props = {
-  type: string;
-  name: string;
-  content: string;
-  isRequired: boolean;
+  children: ReactNode;
 };
 
-export default function LabelInput({ type, name, isRequired, content }: Props) {
-  return (
-    <div className="flex flex-col">
-      <Label input={name} content={content} />
-      <Input type={type} name={name} isRequired={isRequired} />
-    </div>
-  );
+export default function LabelInput({ children }: Props) {
+  return <div className="flex flex-col">{children}</div>;
 }
